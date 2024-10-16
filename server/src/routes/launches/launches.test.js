a1011be7046cb633d5fd47e5474fffaf7434bd1e
hoan301298 <hoantran301298@gmail.com>
2024-10-16 18:58:50 +0300

    Edit test command

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../app.js');
+const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../service/mongo');
 const { loadPlanetsData } = require('../../models/planets.model');
 
